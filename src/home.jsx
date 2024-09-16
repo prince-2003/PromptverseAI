e581c5f84f444f5517708251ce1a3afc66e98a94
Prince <prince_22410@aitpune.edu.in>
2024-09-16 22:03:37 +0530

    gallery section for mobile

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import Infinitescroll from "./components/infintescroll";
 import ButtonPrimary from "./components/button1";
 import ButtonSecondary from "./components/button2";
 
 function Home() {
+
+  const images = [
+    '/Assests/Column.png',
+    '/Assests/1Column.png',
+    '/Assests/2Column.png',
+    '/Assests/3Column.png',
+    '/Assests/4Column.png',
+  ];
+  
+    const [indx, setIndx] = useState(0);
+
+   
+    const handlePrev = () => {
+      setIndx((prevIndx) => (prevIndx === 0 ? images.length - 1 : prevIndx - 1));
+    };
+  
+    const handleNext = () => {
+      setIndx((prevIndx) => (prevIndx === images.length - 1 ? 0 : prevIndx + 1));
+    };
+
   return (
     <>
       <div className="flex flex-col gap-4 text-white justify-center items-center">
@@ -52,17 +73,53 @@ function Home() {
         </div>
       </div>
       <div className="mt-16 flex justify-center w-full">
-        <div className="flex gap-4 items-center w-[80%]">
+        <div className="hidden md:flex gap-4 items-center w-[80%]">
           <div>
             <img src="/Assests/Column.png" alt="Column" />
           </div>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="flex md:grid md:grid-cols-2 gap-4">
             <img src="/Assests/1Column.png" alt="Column" />
             <img src="/Assests/2Column.png" alt="Column" />
             <img src="/Assests/3Column.png" alt="Column" />
             <img src="/Assests/4Column.png" alt="Column" />
           </div>
         </div>
+        <div className="flex justify-center md:hidden gap-4 items-center w-[80%] relative">
+        <div className="absolute left-0 block md:hidden backdrop-filter backdrop-blur-lg rounded-[20px] p-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+            height="18"
+            width="18"
+            fill="white"
+            onClick={handlePrev}
+            className="cursor-pointer"
+            style={{ transform: 'rotate(180deg)' }}
+          >
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
+        </div>
+
+       
+        <div>
+          <img src={images[indx]} alt={`Image ${indx + 1}`} />
+        </div>
+
+        
+        <div className="absolute right-0 block md:hidden backdrop-filter backdrop-blur-lg rounded-[20px] p-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+            height="18"
+            width="18"
+            fill="white"
+            onClick={handleNext}
+            className="cursor-pointer"
+          >
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
+        </div>
+      </div>
       </div>
       <div className=" mt-16 flex flex-col gap-4 text-white justify-center items-center">
         <div className="text-[1.5rem] font-[400] leading-4">
